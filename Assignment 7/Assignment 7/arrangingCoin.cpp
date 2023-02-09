c438a27f7d8898958bbed7ddce4a48e7230f4898
Ayush Raj <ayushr2912btechcse2025@kccitm.edu.in>
2023-02-09 15:22:59 +0530

    que 3

---
+++ Assignment 7/Assignment 7/arrangingCoin.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int arrangeCoins(int n)
+    {
+        int k = n;
+        long long i = 1;
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        while (k > 0)
+        {
+            i++;
+            k -= i;
+        }
+        return i - 1;
+    }
+};
